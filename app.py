e1b7232a90ea195aa49942863089d86b55f003de
orojlo <a.orojlo@gmail.com>
2022-05-19 23:51:02 +0430

    SQLAlchemy

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
-from flask import Flask
+from venv import create
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100))
+    complete = db.Column(db.Boolean)
 
 @app.route('/')
-
-
 def index():
-    return "Hey"
+    todo_list = Todo.query.all()
+    print(todo_list)
+    return render_template('base.html', todo_list=todo_list)
+
+if __name__ == "__main__":
+    db.create_all()
+
+    app.run(debug=True)
