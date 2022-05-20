9a8ac26017286c2c99e09bd4f4b35a2fcb8f6d0c
orojlo <a.orojlo@gmail.com>
2022-05-20 21:21:17 +0430

    add update actions

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from venv import create
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 
@@ -19,6 +18,36 @@ def index():
     todo_list = Todo.query.all()
     print(todo_list)
     return render_template('base.html', todo_list=todo_list)
+
+
+@app.route("/add", methods=["POST"])
+def add():
+    # Add New Item
+    title = request.form.get('title')
+    new_todo = Todo(title=title, complete=False)
+    db.session.add(new_todo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+
+@app.route("/update/<int:todo_id>")
+def update(todo_id):
+    # Update
+    todo = Todo.query.filter_by(id=todo_id).first()
+    todo.complete = not todo.complete
+    db.session.commit()
+    return redirect(url_for("index"))
+
+
+@app.route("/delete/<int:todo_id>")
+def delete(todo_id):
+    # Update
+    todo = Todo.query.filter_by(id=todo_id).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect(url_for("index"))
+
+
 
 if __name__ == "__main__":
     db.create_all()
